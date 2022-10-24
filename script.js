9df8ffcdf179a0e6046570bb9de538a5c9f25d8e
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2022-10-24 15:45:17 -0400

    added more variables to the top. Created multiple functions to run through the questions, have them created with the options and then displayed on the screen. Also created a function to have the countdown start once the user clicks on the start button. Added another function that has the highscore beging at 200 and for every guess the user takes, it will get subtracted by 20. If the user completes all 5 questions without getting anything wrong then they shall recieve a 100 for the grade

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //assigning some variables
+let timeLeft = 80;
+let quizScore = 200;
+let index = 1;
+let games = [];
 let countdownEl = document.getElementById("countdown");
 let startQuizEl = document.getElementById("startQuiz");
 let questionsEl = document.getElementById("questions");
@@ -57,3 +61,69 @@ let questionList = [
     correctAnswer: "let"
 } 
 ];
+
+//when the start button is clicked, the countdown will begin and the questions will be asked
+startQuizEl.addEventListener("click", function () {
+  countdown();
+  renderQuestions(questionList[0]);
+});
+
+//This is so the game continues to ask questions as you complete each question until you get to the last one
+questionsEl.addEventListener("click", function(event) {
+  if (index == 5) {
+      alert("Thank you for Testing your JavaScript Knowledge, the game is over. Please submit your score a refresh the browse to try again");
+  } else if (event.target.textContent != questionList[index - 1].correctAnswer) {
+    timeLeft -= 10;
+  } else {
+    index++; 
+    console.log(timeLeft);
+    renderQuestions(questionList[index - 1]);
+  }
+});
+//The User starts with 200 points and for each question he gets wrong, 20 is deducted
+questionsEl.addEventListener("click", function(event) {
+  if (event.target.textContent === questionList[index - 1].correctAnswer) {
+      quizScore += 20;
+      scoreEl.textContent = "Your score = " + quizScore
+  }else {
+      quizScore -= 20;
+      scoreEl.textContent = "Your score = " + quizScore
+      setTimeout(questionList[index - 1]);
+  }
+});
+ // Timer that counts down from 80 seconds
+ function countdown() {
+  let timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      countdownEl.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+// This functions is used to start the quiz and so the viewer could see the questions on display
+function renderQuestions(validQuestion) {
+  questionsEl.innerHTML = "";
+
+  // creates the questions and options and then appends them to the screen. Saw a youtube video on how to do this
+  let questionAsked = document.createElement("p");
+  let optionsList = document.createElement("ol");
+  let option1 = document.createElement("li");
+  let option2 = document.createElement("li");
+  let option3 = document.createElement("li");
+  let option4 = document.createElement("li");
+
+  questionAsked.textContent = validQuestion.question;
+  option1.textContent = validQuestion.options[1];
+  option2.textContent = validQuestion.options[2];
+  option3.textContent = validQuestion.options[3];
+  option4.textContent = validQuestion.options[4];
+
+  questionsEl.append(questionAsked);
+  questionsEl.append(optionsList);
+  optionsList.append(option1);
+  optionsList.append(option2);
+  optionsList.append(option3);
+  optionsList.append(option4);
+}
